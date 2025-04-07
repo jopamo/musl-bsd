f98f66ef7af19f196ca239e5eb551b20229f428b
Paul <p@1g4.org>
2025-04-07 00:14:26 -0500

    heap-buffer-overflow in fts_build

--- src/test_all.c
+++ src/test_all.c
@@ -17,32 +17,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <time.h>       /* for printing timestamps if you like */
-#include <sys/stat.h>   /* if you want to show st_mode, etc. */
+#include <time.h>     /* for printing timestamps if you like */
+#include <sys/stat.h> /* if you want to show st_mode, etc. */
 
 #include "fts.h"
 #include "obstack.h"
 
 /* If you want to use standard malloc/free for obstack, define these macros: */
 #ifndef obstack_chunk_alloc
-#  define obstack_chunk_alloc  malloc
+#define obstack_chunk_alloc malloc
 #endif
 #ifndef obstack_chunk_free
-#  define obstack_chunk_free   free
+#define obstack_chunk_free free
 #endif
 
 /* Forward declarations for test functions */
-static void test_fts(int argc, char *argv[]);
+static void test_fts(int argc, char* argv[]);
 static void test_obstack(void);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     /* If you run with arguments, we'll treat them as paths for the FTS test.
      * Otherwise, we default to "." for demonstration. */
     if (argc > 1) {
         test_fts(argc, argv);
-    } else {
-        char *fake_argv[2];
+    }
+    else {
+        char* fake_argv[2];
         fake_argv[0] = ".";
         fake_argv[1] = NULL;
         test_fts(1, fake_argv);
@@ -59,13 +59,12 @@ int main(int argc, char *argv[])
  *  Demonstrate usage of fts_open, fts_read, fts_children, fts_set, fts_close
  *  with more verbose debug prints.
  *****************************************************************************/
-static void test_fts(int argc, char *argv[])
-{
-    FTS *fts_handle;
-    FTSENT *ent;
-    int fts_options = FTS_NOCHDIR | FTS_PHYSICAL;  /* example options */
+static void test_fts(int argc, char* argv[]) {
+    FTS* fts_handle;
+    FTSENT* ent;
+    int fts_options = FTS_NOCHDIR | FTS_PHYSICAL; /* example options */
 
-    char **paths = NULL;
+    char** paths = NULL;
     int i;
     int offset = 0;
 
@@ -73,7 +72,7 @@ static void test_fts(int argc, char *argv[])
     if (argc > 1) {
         offset = 1;
     }
-    paths = calloc((argc - offset) + 1, sizeof(char *));
+    paths = calloc((argc - offset) + 1, sizeof(char*));
     if (!paths) {
         fprintf(stderr, "Out of memory building path array!\n");
         return;
@@ -108,52 +107,49 @@ static void test_fts(int argc, char *argv[])
          * ent->fts_level is the depth in the tree,
          * ent->fts_errno if there's an error, etc.
          */
-        printf("  [fts_read] Node at level=%d: \"%s\"\n",
-               ent->fts_level, ent->fts_path);
+        printf("  [fts_read] Node at level=%d: \"%s\"\n", ent->fts_level, ent->fts_path);
 
         /* If we have stat info stored, we could show st_mode, etc.
          * That depends on fts usage. E.g.:
          */
         if (ent->fts_statp) {
-            struct stat *st = ent->fts_statp;
-            printf("    Dev/Ino: %lu/%lu, Mode: 0%o\n",
-                   (unsigned long)st->st_dev,
-                   (unsigned long)st->st_ino,
+            struct stat* st = ent->fts_statp;
+            printf("    Dev/Ino: %lu/%lu, Mode: 0%o\n", (unsigned long)st->st_dev, (unsigned long)st->st_ino,
                    (unsigned)st->st_mode);
         }
 
         /* Show the fts_info classification in a more descriptive manner. */
         switch (ent->fts_info) {
-        case FTS_D:
-            printf("    => Directory (pre-order)\n");
-            break;
-        case FTS_DP:
-            printf("    => Directory (post-order)\n");
-            break;
-        case FTS_F:
-            printf("    => Regular file\n");
-            break;
-        case FTS_SL:
-            printf("    => Symlink\n");
-            break;
-        case FTS_ERR:
-            printf("    => ERROR (errno=%d)\n", ent->fts_errno);
-            break;
-        case FTS_DNR:
-            printf("    => Directory not readable (errno=%d)\n", ent->fts_errno);
-            break;
-        case FTS_NS:
-            printf("    => No stat info available (errno=%d)\n", ent->fts_errno);
-            break;
-        case FTS_DC:
-            printf("    => Directory cycle!\n");
-            break;
-        case FTS_DOT:
-            printf("    => Dot directory (\".\" or \"..\")\n");
-            break;
-        default:
-            printf("    => Other node type (fts_info=%d)\n", ent->fts_info);
-            break;
+            case FTS_D:
+                printf("    => Directory (pre-order)\n");
+                break;
+            case FTS_DP:
+                printf("    => Directory (post-order)\n");
+                break;
+            case FTS_F:
+                printf("    => Regular file\n");
+                break;
+            case FTS_SL:
+                printf("    => Symlink\n");
+                break;
+            case FTS_ERR:
+                printf("    => ERROR (errno=%d)\n", ent->fts_errno);
+                break;
+            case FTS_DNR:
+                printf("    => Directory not readable (errno=%d)\n", ent->fts_errno);
+                break;
+            case FTS_NS:
+                printf("    => No stat info available (errno=%d)\n", ent->fts_errno);
+                break;
+            case FTS_DC:
+                printf("    => Directory cycle!\n");
+                break;
+            case FTS_DOT:
+                printf("    => Dot directory (\".\" or \"..\")\n");
+                break;
+            default:
+                printf("    => Other node type (fts_info=%d)\n", ent->fts_info);
+                break;
         }
 
         /*
@@ -162,23 +158,26 @@ static void test_fts(int argc, char *argv[])
          */
         if (ent->fts_info == FTS_D) {
             printf("    => Attempting fts_children on this directory...\n");
-            FTSENT *ch = fts_children(fts_handle, 0);
+            FTSENT* ch = fts_children(fts_handle, 0);
             if (!ch) {
                 /* Either no children or an error. Check errno. */
                 if (errno) {
                     fprintf(stderr, "      fts_children error: %s\n", strerror(errno));
-                } else {
+                }
+                else {
                     printf("      (No children in this directory)\n");
                 }
-            } else {
+            }
+            else {
                 /* We can iterate the returned linked list of children. */
-                FTSENT *c = ch;
+                FTSENT* c = ch;
                 while (c) {
                     /* Example: if it's a dot dir, skip it. */
                     if (c->fts_info == FTS_DOT) {
                         printf("      Child: \"%s\" is DOT, skipping.\n", c->fts_name);
                         fts_set(fts_handle, c, FTS_SKIP);
-                    } else {
+                    }
+                    else {
                         printf("      Child: \"%s\"\n", c->fts_name);
                     }
                     c = c->fts_link;
@@ -189,8 +188,7 @@ static void test_fts(int argc, char *argv[])
 
     /* If the loop ended because of an error, errno will be nonzero. */
     if (errno != 0) {
-        fprintf(stderr, "[FTS TEST] fts_read returned NULL with errno: %s\n",
-                strerror(errno));
+        fprintf(stderr, "[FTS TEST] fts_read returned NULL with errno: %s\n", strerror(errno));
     }
 
     /* Close the FTS handle. */
@@ -206,8 +204,7 @@ static void test_fts(int argc, char *argv[])
  * test_obstack
  *  Demonstrate usage of obstack: building string objects, etc. with extra info.
  *****************************************************************************/
-static void test_obstack(void)
-{
+static void test_obstack(void) {
     struct obstack myobstack;
     char *str1, *str2, *str3;
     size_t total_used;
@@ -237,13 +234,12 @@ static void test_obstack(void)
 
     /* Show how many bytes the obstack has allocated in total. */
     total_used = _obstack_memory_used(&myobstack);
-    printf("[OBSTACK TEST] Currently using %zu bytes in obstack.\n", (size_t) total_used);
+    printf("[OBSTACK TEST] Currently using %zu bytes in obstack.\n", (size_t)total_used);
 
     /* Example: free everything in the obstack with obstack_free(..., NULL). */
     printf("[OBSTACK TEST] Freeing all data in the obstack...\n");
     obstack_free(&myobstack, NULL);
-    printf("  => Freed all data. Now _obstack_memory_used = %zu\n",
-           (size_t)_obstack_memory_used(&myobstack));
+    printf("  => Freed all data. Now _obstack_memory_used = %zu\n", (size_t)_obstack_memory_used(&myobstack));
 
     /* The obstack can still be used further if desired. */
     printf("[OBSTACK TEST] Done.\n\n");
