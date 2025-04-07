242111b44e8ddaefaecbda64190082b6d8d5042c
Paul <p@1g4.org>
2025-04-07 06:00:26 -0500

    include libargp

--- src/test_all.c
+++ src/test_all.c
@@ -1,10 +1,11 @@
+#include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include <errno.h>
-#include <time.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <argp.h>
 
 #include "fts.h"
 #include "obstack.h"
@@ -16,18 +17,44 @@
 #define obstack_chunk_free free
 #endif
 
+static void test_argp(int argc, char* argv[]);
+
 static void test_fts(int argc, char* argv[]);
 static void test_obstack(void);
+
+const char* argp_program_version = "1.0";
+const char* argp_program_bug_address = "<bug-report@example.com>";
+
+static struct argp_option options[] = {{"help", 'h', 0, 0, "Display this help message", 0},
+                                       {"version", 'v', 0, 0, "Display version information", 0},
+                                       {0}};
+
+static error_t parse_opt(int key, char* arg __attribute__((unused)), struct argp_state* state) {
+    switch (key) {
+        case 'h':
+            argp_state_help(state, stdout, ARGP_HELP_STD_HELP);
+            break;
+        case 'v':
+            printf("Version: %s\n", argp_program_version);
+            break;
+        case ARGP_KEY_ERROR:
+            return EINVAL;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, NULL, "Test `argp` parser.", NULL, NULL, NULL};
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
         test_fts(argc, argv);
+        test_argp(argc, argv);
     }
     else {
-        char* fake_argv[2];
-        fake_argv[0] = ".";
-        fake_argv[1] = NULL;
-        test_fts(1, fake_argv);
+        char* fake_argv[] = {"./program", "--help", NULL};
+        test_argp(2, fake_argv);
     }
 
     test_obstack();
@@ -152,6 +179,18 @@ static void test_fts(int argc, char* argv[]) {
 
     free(paths);
     printf("[FTS TEST] Done with file hierarchy.\n\n");
+}
+
+static void test_argp(int argc, char* argv[]) {
+    printf("[ARGP TEST] Testing argp parser with provided arguments...\n");
+
+    error_t err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+    if (err) {
+        printf("[ARGP TEST] Error parsing arguments!\n");
+    }
+    else {
+        printf("[ARGP TEST] Arguments parsed successfully.\n");
+    }
 }
 
 static void test_failed_allocation_handler(void) {
