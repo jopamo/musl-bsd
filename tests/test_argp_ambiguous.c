c17f3fcf31d5e706a7db27f3afccb7cdfe93d693
Paul Moses <p@1g4.org>
2025-11-02 13:26:02 -0600

    Add test for ambiguous argp options Add new test file tests/test_argp_ambiguous c to verify argp behavior with ambiguous long Refactor test_fts c with improved error handling and colored output macros Add missing includes and helper functions for file system operations Update test_obstack c to define _XOPEN_SOURCE for POSIX compliance

---
+++ tests/test_argp_ambiguous.c
@@ -0,0 +1,27 @@
+#include <argp.h>
+#include <errno.h>
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    (void)key;
+    (void)arg;
+    (void)state;
+    return 0;
+}
+
+int main(void) {
+    static const struct argp_option options[] = {
+        {"foo", 'f', 0, 0, "foo option", 0},
+        {"fob", 'b', 0, 0, "fob option", 0},
+        {0}
+    };
+
+    static const struct argp argp = {options, parse_opt, NULL, NULL, NULL, NULL, NULL};
+
+    char prog[] = "prog";
+    char opt[] = "--fo";
+    char* argv[] = {prog, opt, NULL};
+    int argc = 2;
+
+    error_t err = argp_parse(&argp, argc, argv, ARGP_NO_EXIT | ARGP_NO_ERRS, NULL, NULL);
+    return err == EINVAL ? 0 : 1;
+}
